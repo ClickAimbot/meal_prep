ad4c5c8ab26ff66180346a99ec083f06621753ce
Austin <austin.amaral3@gmail.com>
2023-11-17 12:33:53 -0800

    wip

--- frontend/src/Pages/Home/home.js
+++ frontend/src/Pages/Home/home.js
@@ -34,32 +34,26 @@ export const Home = () => {
   }
 
   return (
-    <div className="app__header app__wrapper section__padding" id="home">
-      <div className="app__wrapper_info">
-        <h1 className="app__header-h1">Made Fresh and Healthy</h1>
-        <p className="p__opensans" style={{ margin: '2rem 0', color: 'black'}} >
-          Check out our new Protein Shake.
-        </p>
-        <button className="custom__button" onClick={routeToPlans}>Explore Plans</button>
+     <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <h1>Made Fresh and Healthy</h1>
+        <p className="p__opensans" style={{ color: 'black', marginTop: '2rem' }}>Try our new Protein Shake!</p>
+        <button className="custom__button" onClick={routeToPlans}>View Plans</button>
       </div>
-
-      <div className="app__gallery flex__center">
-        <div className='app__gallery-images'>
-          <div className='app__gallery-images_container' ref={scrollRef}>
-            {galleryImages.map((image, index) => (
-              <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
-                <img src={image} alt="gallery" />
-                <BsInstagram className="gallery__image-icon" />
-              </div>
-            ))}
-          </div>
-
-          <div className="app__gallery-images_arrows">
-            <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')}/>
-            <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')}/>
-          </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort className="gallery__arrow-icon" onClick={() => scroll('left')} />
+          <BsArrowRightShort className="gallery__arrow-icon" onClick={() => scroll('right')} />
         </div>
       </div>
-    </div>
-  )
+    </div> 
+  );
 }
